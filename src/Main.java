49fec91310f72e7f5e991328333e4c589a4423fa
QorxmazQarayev <qorxmazqarayev60@gmail.com>
2023-09-19 21:27:31 +0400

    first

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack <String> stack=new Stack<>();
+        stack.pop();
+        stack.peek();
+        stack.push("Ad");
+        stack.size();
+
+    }
+}
---
+++ src/Stack.java
@@ -0,0 +1,11 @@
+public class Stack<T>{
+    public void push(T ad){
+
+    }
+    public void peek(){
+
+    }
+    public void size(){
+
+    }
+}
